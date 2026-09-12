ad1e7eb29763e43bc7d4939a12ac318fbe84efea
Rafi Indra Pramudhito Zuhayr <zuudevs@gmail.com>
2026-09-12 22:07:18 +0700

    test: add C++17 differential test target helper

--- cmake/modules/testing.cmake
+++ cmake/modules/testing.cmake
@@ -19,3 +19,12 @@ function(add_test_target name)
 	)
 	gtest_discover_tests(${name})
 endfunction()
+
+function(add_cxx17_test_target name)
+	add_test_target(${name} ${ARGN})
+	set_target_properties(${name} PROPERTIES
+		CXX_STANDARD 17
+		CXX_STANDARD_REQUIRED ON
+		CXX_EXTENSIONS OFF
+	)
+endfunction()
