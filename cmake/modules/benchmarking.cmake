a84dd2b6e2a21d9d582360dbec0d7a7f19713619
Rafi Indra Pramudhito Zuhayr <zuudevs@gmail.com>
2026-09-12 21:40:51 +0700

    build: separate C++11 core from C++17 benchmarks (#1)
    * build: make C++11 the project default
    
    * build: require C++17 only for benchmarks
    
    * build: enforce C++11 for unit tests
    
    * ci: validate C++11 core with C++17 benchmarks

--- cmake/modules/benchmarking.cmake
+++ cmake/modules/benchmarking.cmake
@@ -2,6 +2,7 @@ include(${CMAKE_SOURCE_DIR}/cmake/packages/google-benchmark.cmake)
 	
 function(add_benchmark_target name)
 	add_executable(${name} ${ARGN})
+	target_compile_features(${name} PRIVATE cxx_std_17)
 	target_link_libraries(${name} 
 		PRIVATE 
 			benchmark::benchmark_main
